1840594b23c98d324f68ea4e6a4c4338e67bb125
kei-kamiguchi <56471454+kei-kamiguchi@users.noreply.github.com>
2022-02-15 20:03:51 +0900

    Update task_spec.rb

--- system/task_spec.rb
+++ system/task_spec.rb
@@ -1,22 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe do
-  describe '1.Active Strageを使用していること' do
-    before do
+  describe do
+    it '1. `profile_image`カラム用の登録フォームが存在すること' do
       visit new_user_path
-      fill_in 'user_name', with: 'sample'
-      fill_in 'user_email', with: 'user@gmail.com'
-      fill_in 'user_password', with: 'password'
-      fill_in 'user_password_confirmation', with: 'password'
-      attach_file 'user[profile_image]', "#{Rails.root}/spec/fixtures/images/スクリーンショット 2021-01-08 18.33.25のコピー.png"
-      find('input[type="submit"]').click
-    end
-    let!(:user){User.take}
-    it '画像がActive Strageに保存されていること' do
-      expect(user.profile_image.class).to eq ActiveStorage::Attached::One
+      expect(find('input[name="user[profile_image]"]')).to be_visible
     end
   end
-  describe '2.関連付けに使用するカラム名に`profile_image`が使用されていること' do
+  describe do
     before do
       visit new_user_path
       fill_in 'user_name', with: 'sample'
@@ -26,48 +17,31 @@ RSpec.describe do
       attach_file 'user[profile_image]', "#{Rails.root}/spec/fixtures/images/スクリーンショット 2021-01-08 18.33.25のコピー.png"
       find('input[type="submit"]').click
     end
-    let!(:user){User.take}
-    it 'has_one_attached :profile_imageが定義されていること' do
-      expect(user.profile_image).to be_attached
+    it '2. アカウント登録時にプロフィール画像を登録できること' do
+      expect(page).to have_content 'アカウントを登録しました。'
     end
-  end
-  describe '3.プロフィール画像を選択してユーザ登録した際、詳細画面に遷移させ、プロフィール画像を表示されること' do
-    before do
-      visit new_user_path
-      fill_in 'user_name', with: 'sample'
-      fill_in 'user_email', with: 'user@gmail.com'
-      fill_in 'user_password', with: 'password'
-      fill_in 'user_password_confirmation', with: 'password'
-      attach_file 'user[profile_image]', "#{Rails.root}/spec/fixtures/images/スクリーンショット 2021-01-08 18.33.25のコピー.png"
-      find('input[type="submit"]').click
+    it '3. Active Strageが使用されていること' do
+      expect(User.last.profile_image.class).to eq ActiveStorage::Attached::One
     end
-    let!(:user){User.take}
-    it 'プロフィール画像を選択してユーザ登録した際、詳細画面に遷移すること' do
-      sleep 1
-      expect(current_path).to eq user_path(user.id)
-    end
-    it 'プロフィール画像を選択してユーザ登録した際、プロフィール画像を表示されること' do
+    it '4. プロフィール画像を選択してユーザ登録した際、詳細画面に遷移させ、プロフィール画像を表示されること' do
+      expect(current_path).to eq user_path(User.last.id)
       # 画像の全体パスだと投稿時刻によって変化するため、変化しない一部比較しテスト
-      sleep 1
       expect(page.find('img')['src']).to have_content '202021-01-08%2018.33.25%E3%81%AE%E3%82%B3%E3%83%94%E3%83%BC.png'
     end
   end
-  describe '4.プロフィール画像を選択せずにユーザ登録した際、エラーを発生させずに詳細画面に遷移させること' do
-    before do
+  describe do
+    it '5. プロフィール画像を選択せずにユーザ登録した際、エラーを発生させずに詳細画面に遷移させること' do
       visit new_user_path
       fill_in 'user_name', with: 'sample'
       fill_in 'user_email', with: 'user@gmail.com'
       fill_in 'user_password', with: 'password'
       fill_in 'user_password_confirmation', with: 'password'
       find('input[type="submit"]').click
-    end
-    let!(:user){User.take}
-    it 'プロフィール画像を選択せずにユーザ登録した際、詳細画面に遷移すること' do
-      expect(current_path).to eq user_path(user.id)
+      expect(current_path).to eq user_path(User.last.id)
     end
   end
-  describe '5.ユーザ登録した際、Action Mailerを使ってそのユーザにメールを送信すること' do
-    it 'ユーザ登録したユーザにメールが送信される' do
+  describe do
+    it '6. アカウント登録した際、Action Mailerを使ってそのユーザにメールを送信すること' do
       visit new_user_path
       fill_in 'user_name', with: 'sample'
       fill_in 'user_email', with: 'user@gmail.com'
@@ -81,8 +55,8 @@ RSpec.describe do
       expect(email.to).to eq ['user@gmail.com']
     end
   end
-  describe "6.手順2で指定したメール文章が送信されていること" do
-    it "送信されるメール情報が意図通りであること" do
+  describe do
+    it "7. 要件で指定したメール文章が送信されていること" do
       visit new_user_path
       fill_in 'user_name', with: 'sample'
       fill_in 'user_email', with: 'user@gmail.com'
@@ -99,8 +73,8 @@ RSpec.describe do
       expect(email.decoded).to include "ユーザ登録が完了しました。"
     end
   end
-  describe '7.Acitve Jobを実装し、`deliever_later`メソッドを使って非同期でメールを送信すること' do
-    it 'ユーザ登録時にキューが追加されること' do
+  describe do
+    it '8. Acitve Jobを実装し、`deliever_later`メソッドを使って非同期でメールを送信すること' do
       visit new_user_path
       fill_in 'user_name', with: 'sample'
       fill_in 'user_email', with: 'user@gmail.com'
