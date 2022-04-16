216ed8574019c21e67989174de318ff24fb57af4
kei-kamiguchi <56471454+kei-kamiguchi@users.noreply.github.com>
2022-04-16 19:58:15 +0900

    Update my_formatter.rb

--- my_formatter.rb
+++ my_formatter.rb
@@ -1,5 +1,5 @@
 class MyFormatter
-  RSpec::Core::Formatters.register self, :example_passed, :example_failed, :start
+  RSpec::Core::Formatters.register self, :example_passed, :example_failed, :start, :stop
   def initialize(output)
     @output = output
   end
@@ -14,5 +14,11 @@ class MyFormatter
 
   def example_failed(notification)
     @output << "- [ ] ❌ #{notification.example.description}\n"
+  end
+  
+  def stop(notification)
+    if notification.examples.count == 0
+      @output << "## Evaluation could not be performed. Please check for syntax errors in your code.\n### Common syntax errors\n- Typos in code.\n- The `end` is missing."
+    end
   end
 end
